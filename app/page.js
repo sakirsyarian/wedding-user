aee5299d819b0c906f74ce243eebd70f954f88a9
Ahmad Sarian <sakirsyarian@gmail.com>
2023-09-21 14:46:39 +0700

    feat: Price section for landing page

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ import Feature from "./component/Feature";
 import Tutorial from "./component/Tutorial";
 import Template from "./component/Template";
 import Testimoni from "./component/Testimoni";
+import Price from "./component/Price";
 
 // css
 const defaultCaontainer = ["py-5", "container", "space-y-20", "text-slate-500"];
@@ -22,6 +23,7 @@ export default function Home() {
         <Tutorial />
         <Template />
         <Testimoni />
+        <Price />
       </div>
     </>
   );
